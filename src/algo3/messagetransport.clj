d99b28d43562beab3309f5393fa2c5d531493e29
Perttu Auramo <perttu.auramo@gmail.com>
2013-04-06 23:45:13 +0300

    refactored server stuff into util file messagetransport

---
+++ src/algo3/messagetransport.clj
@@ -0,0 +1,14 @@
+(ns algo3.messagetransport)
+
+(use 'lamina.core 'aleph.tcp 'gloss.core)
+
+(def msg-separator "\r\n")
+
+(defn create-handler [msg-callback]
+  (fn [ch client-info]
+    (receive-all ch #(msg-callback ch %))))
+
+(defn start-server [msg-callback port]
+  (println "Starting server on port" port)
+  (start-tcp-server (create-handler msg-callback) {:port port, :frame (string :utf-8 :delimiters [msg-separator])}))
+
