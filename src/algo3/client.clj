fc58415a01e4c8eaa4cccb848cf2b2f004f2281b
Perttu Auramo <perttu.auramo@gmail.com>
2013-04-07 11:02:08 +0300

    send messages constantly to test that the connection works after initial send/receive

--- src/algo3/client.clj
+++ src/algo3/client.clj
@@ -2,11 +2,13 @@
   (:use 
         [algo3.messagetransport]))
 
-(defn send-msg []
-  (let [ch (start-client-channel "localhost" 10000)]
-    (println "sending message")
-    (send-message ch {:foo "bar"})
-    (println "sent the message, got reply:")
-    (println (read-message ch))))
+(defn start-sending []
+  (loop [channel (start-client-channel "localhost" 10000)]
+    (println "Sending message")
+    (send-message channel {:foo "bar"})
+    (println "Got reply:")
+    (println (read-message channel))
+    (Thread/sleep 3000)
+    (recur channel)))
 
-(defn -main[] (send-msg))
+(defn -main[] (start-sending))
